09ae0c1cf6062a1aa407f55c3141d1e9cb431189
Nicolas-BELLANGER <146709327+Nicolas-BELLANGER@users.noreply.github.com>
2023-10-02 10:20:51 +0200

    Add files via upload

---
+++ exercice3.java
@@ -0,0 +1,9 @@
+public class exercice3{ 
+    public static void main(String[] args) {
+        String name = "Halil";
+        Integer age = 27;
+        String job = "developpeur";
+
+        System.out.println("Je m'appelle " + name + ", j'ai " + age + " ans et je suis " + job);
+    }
+}
