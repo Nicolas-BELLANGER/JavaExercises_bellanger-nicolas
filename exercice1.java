8789e36efc7583c5e0d815644e5b81bbc065885d
Nicolas-BELLANGER <146709327+Nicolas-BELLANGER@users.noreply.github.com>
2023-10-02 10:04:42 +0200

    Add files via upload

---
+++ exercice1.java
@@ -0,0 +1,9 @@
+public class vars{ 
+    public static void main(String[] args) {
+        String name = "Alex";
+        Integer age = 25;
+        String concatenation = name + age;
+        System.out.println(concatenation);
+
+    }
+}
