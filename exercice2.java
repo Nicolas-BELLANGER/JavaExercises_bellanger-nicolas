f2b955568248c35aaf09e85dc3fd6591ec771116
Nicolas-BELLANGER <146709327+Nicolas-BELLANGER@users.noreply.github.com>
2023-10-02 10:10:43 +0200

    Add files via upload

---
+++ exercice2.java
@@ -0,0 +1,10 @@
+public class exercice2{ 
+    public static void main(String[] args) {
+        Double price = 19.99;
+        Integer quantity = 12;
+        Double total = price*quantity;
+
+        System.out.println(total);
+
+    }
+}
