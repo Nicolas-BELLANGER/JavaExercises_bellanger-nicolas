076fd6aafa4dd42d8d97c1a3cddd783957a26d9b
Nicolas-BELLANGER <146709327+Nicolas-BELLANGER@users.noreply.github.com>
2023-10-02 11:13:45 +0200

    Add files via upload

---
+++ exercice4.java
@@ -0,0 +1,11 @@
+public class exercice4 {
+    public static void main(String[] args) {
+        String str = "Hello World !";
+        Integer i = 0;
+
+        while(i < str.length()){
+            System.out.print(str.charAt(i) + " ");
+            i++;
+        }
+        }
+    }    
