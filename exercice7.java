6fac302d928630d82342fbedb00e96b59a7adf6f
Nicolas-BELLANGER <146709327+Nicolas-BELLANGER@users.noreply.github.com>
2023-10-02 11:38:24 +0200

    Add files via upload

---
+++ exercice7.java
@@ -0,0 +1,7 @@
+public class exercice7 {
+    public static void main(String[] args) {
+        for (Integer i = 0; i < 11; i++){
+            System.out.println(i + " x 5 = "+ i*5);
+        }
+    }
+}
